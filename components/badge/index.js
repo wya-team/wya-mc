bc3fdd85ec2eb85fbee596d76aedf64bc6f2e70c
JDEast1029 <852778359@qq.com>
2018-05-30 11:34:59 +0800

    add: badge

--- components/badge/index.js
+++ components/badge/index.js
@@ -1,3 +1,20 @@
 Component({
-
+    properties: {
+        color: {
+            type: String,
+            value: '#ffffff'
+        },
+        backgroundColor: {
+            type: String,
+            value: '#ff4444'
+        },
+        fontSize: {
+            type: Number,
+            value: 20
+        },
+        boxShadow: {
+            type: String,
+            value: '0 0 0 2px #fff'
+        }
+    }
 })
