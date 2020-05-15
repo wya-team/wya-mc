bd6cf68bf7170a9f93ad9b1ee9b42bec62b04186
蒋东 <852778359@qq.com>
2020-05-15 15:28:09 +0800

    feat: 更新mc-list & mc-list-item

--- src/common/component.js
+++ src/common/component.js
@@ -1,7 +1,16 @@
 function McComponent(mcOptions) {
 	if (mcOptions === undefined) { mcOptions = {}; }
+	const { externalClasses = [], ...restOpts } = mcOptions;
+	if (!externalClasses.includes('mc-class')) {
+		externalClasses.push('mc-class');
+	}
 	let options = {
-		...mcOptions,
+		...restOpts,
+		externalClasses,
+		properties: {
+			...mcOptions.properties,
+			mcStyle: String
+		},
 		methods: {
 			...mcOptions.methods,
 			$emit(event, options) {
