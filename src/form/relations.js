dc7b050a5488c6a1cebf3c0057afa5737121b6db
蒋东 <852778359@qq.com>
2020-10-14 11:09:42 +0800

    fix: date-picker关联form-item

--- src/form/relations.js
+++ src/form/relations.js
@@ -19,6 +19,16 @@ export const formComponents = {
 			this.children = (this.children || []).filter((it) => it !== child);
 		}
 	},
+	'../date-picker/index': {
+		type: 'descendant',
+		linked(child) {
+			this.children = this.children || [];
+			this.children.push(child);
+		},
+		unlinked(child) {
+			this.children = (this.children || []).filter((it) => it !== child);
+		}
+	},
 	'../cascader/index': {
 		type: 'descendant',
 		linked(child) {
