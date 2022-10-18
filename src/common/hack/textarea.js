c735a67d5cb389b5299db5d051100789c4f86199
蒋东 <852778359@qq.com>
2022-10-18 11:08:34 +0800

    feat: 多层popup后关闭最顶部的弹框，导致textarea显示出来

--- src/common/hack/textarea.js
+++ src/common/hack/textarea.js
@@ -20,24 +20,55 @@ class TextareaHack {
 		this.instanceList = this.instanceList.filter((it) => it.data.uuid === instance.data.uuid);
 	}
 
-	toggle(visible, data) {
-		if (data) {
-			if (!(data instanceof Array)) {
-				data = [data];
+	toggleInCollapseItem(visible, data) {
+		if (!(data instanceof Array)) {
+			data = [data];
+		}
+		data.forEach(textInstance => {
+			if (textInstance.data.viewPlaceholder) {
+				textInstance.setData({ shouldHide: visible });
 			}
-			data.forEach(instance => {
-				instance.data.viewPlaceholder && instance.setData({
-					shouldHide: visible
-				});
+		});
+	}
+
+	toggle(visible, popInstance) {
+		if (visible) {
+			this.instanceList.forEach((instance) => {
+				instance.relatedPopList = instance.relatedPopList || [];
+				instance.relatedPopList.push(popInstance);
 			});
 		} else {
 			this.instanceList.forEach((instance) => {
-				if (instance.collaspeItem && !instance.collaspeItem.data.isActive && !visible) return;
-				instance.data.viewPlaceholder && instance.setData({
-					shouldHide: visible
-				});
+				instance.relatedPopList = (instance.relatedPopList || []).filter((it) => it === popInstance);
 			});
 		}
+
+		this.instanceList.forEach((instance) => {
+			let shouldHide = instance.relatedPopList && instance.relatedPopList.length > 0;
+			if (instance.collaspeItem && !instance.collaspeItem.data.isActive && !shouldHide) return;
+			instance.data.viewPlaceholder && instance.setData({
+				shouldHide
+			});
+		});
+
+		// 原代码，存在多次pop后，再关闭一个pop，textarea就会浮现
+		// if (data) {
+		// 	if (!(data instanceof Array)) {
+		// 		data = [data];
+		// 	}
+		// 	data.forEach(instance => {
+		// 		instance.data.viewPlaceholder && instance.setData({
+		// 			shouldHide: visible
+		// 		});
+		// 	});
+		// } else {
+		// 	this.instanceList.forEach((instance) => {
+		// 		if (instance.collaspeItem && !instance.collaspeItem.data.isActive && !visible) return;
+		// 		instance.data.viewPlaceholder && instance.setData({
+		// 			shouldHide: visible
+		// 		});
+		// 	});
+		// }
 	}
 }
 
